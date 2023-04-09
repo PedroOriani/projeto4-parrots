60e1c1ca898cc4619eca135d4b7cbc17fec6374b
Pedro Oriani <pedro.oriani@gmail.com>
2023-04-09 10:07:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const n = prompt('Bem vindo ao Parrots Card Game.\nCom quantas cartas quer jogar o jogo?');
+
+while (n % 2 !== 0){
+    console.log(n)
+    n = prompt('Bem vindo ao Parrots Card Game.\nCom quantas cartas quer jogar o jogo?');
+}
+
+
+
+//function generateCards(){}
+
+//minhaArray.sort(comparador);
+
+//function comparador() { 
+//	return Math.random() - 0.5; 
+//}
+
